e69de85c63ce8dd19c28f8ac1e1ad9ca04c51072
David Aaron <davidmatthewaaron@gmail.com>
2013-06-11 19:22:37 -0400

    pulling newest version

--- db/migrate/20130611214405_create_users.rb
+++ db/migrate/20130611214405_create_users.rb
@@ -2,8 +2,8 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :username
-      t.string :password
       t.string :email
+      t.string :password_digest
       t.integer :neighborhood_id
 
       t.timestamps
