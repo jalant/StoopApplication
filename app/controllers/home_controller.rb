28e1e1d9de2dbbca9bcc7e538f1410fa445f32d8
David Aaron <davidmatthewaaron@gmail.com>
2013-06-11 23:36:25 -0400

    index map now correct

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,30 +1,30 @@
 class HomeController < ApplicationController
 
 
-  def index
+def index
+    @sales = Sale.all
+    @neighborhoods = Neighborhood.all
+    @users = User.all
 
-    @neighborhood = "Williamsburg"
-
-    maps = Geocoder.search("Williamsburg, Brooklyn, New York")
+    maps = Geocoder.search("Brooklyn, New York")
     lat_lng = maps.first.data["geometry"]['location']
     @map_lat = lat_lng["lat"]
     @map_lng = lat_lng["lng"]
-    # binding.pry
-
-    @sales = Sale.all
 
     @locations = []
 
     @sales.each do |sale|
 
-      if sale.user.neighborhood.name == @neighborhood
+      
         address_tmp = "#{sale.address}, #{sale.city}"
         @marker = Geocoder.search(address_tmp)
         mark_lat = @marker.first.data["geometry"]['location']['lat']
         mark_lng = @marker.first.data["geometry"]['location']['lng']
         @locations << [sale.title, mark_lat, mark_lng]
 
-      end
+      
+
     end
-  end
+
+  end 
 end
