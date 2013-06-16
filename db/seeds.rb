bc290989d856a4f9e646a7dec715b23729748317
David Aaron <davidmatthewaaron@gmail.com>
2013-06-16 00:02:06 -0400

    changing seed file format, adding message if neighborhood has no sales

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,16 +4,22 @@ Item.delete_all
 Sale.delete_all
 User.delete_all
 
-n1, n2, n3, n4, n5, n6 = ['Brooklyn Heights', 'Red Hook', 'Williamsburg', 'Carroll Gardens', 'Greenpoint','Fort Greene'].map do |name|
-	Neighborhood.create(name: name)
-end
 
+
+
+n1 = Neighborhood.create(name: 'Brooklyn Heights')
+n2 = Neighborhood.create(name: 'Red Hook')
+n3 = Neighborhood.create(name: 'Williamsburg')
+n4 = Neighborhood.create(name: 'Carroll Gardens')
+n5 = Neighborhood.create(name: 'Greenpoint')
+n6 = Neighborhood.create(name: 'Fort Greene')
 
 u1 = User.create(username: 'Tanay', password: 'pass123', email: 'tanay@stoopid.com', admin: true)
 u2 = User.create(username: 'David', password: 'pass123', email: 'david@stoopid.com')
 u3 = User.create(username: 'Jamie', password: 'pass123', email: 'jamie@stoopid.com')
 u4 = User.create(username: 'Mike', password: 'pass123', email: 'mike@stoopid.com')
 u5 = User.create(username: 'Ryan', password: 'pass123', email: 'ryan@stoopid.com')
+u6 = User.create(username: 'Bill', password: 'pass123', email: 'bill@stoopid.com')
 
 
 
@@ -23,7 +29,7 @@ s2 = Sale.create(title: 'Red Hook Sale', address: '391 Van Brunt Street', city: 
 s3 = Sale.create(title: 'Williamsburg Sale', address: '170 Bedford Avenue', city: 'Brooklyn', date:'2013-9-1', time: '2pm')
 s4 = Sale.create(title: 'Carroll Gardens Sale', address: '400 Degraw Street', city: 'Brooklyn', date:'2013-10-5', time: '1pm')
 s5 = Sale.create(title: 'Greenpoint Sale', address: '700 Manhattan Avenue', city: 'Brooklyn', date:'2013-11-11', time: '12pm')
-s6 = Sale.create(title: 'Fort Greene Sale', address: '642 address street', city: 'Brooklyn', date:'2013-12-15', time: '1:30pm')
+s6 = Sale.create(title: 'Fort Greene Sale', address: '121 Dekalb Avenue', city: 'Brooklyn', date:'2013-12-15', time: '1:30pm')
 
 
 i1 = Item.create(name: 'television', price: 200, description: '54 inch flat screen')
@@ -38,12 +44,14 @@ n2.users << u2
 n3.users << u3
 n4.users << u4
 n5.users << u5
+n6.users << u6
 
 u1.sales << s1
 u2.sales << s2
 u3.sales << s3
 u4.sales << s4
-u5.sales << s5 << s6
+u5.sales << s5 
+u6.sales << s6
 
 s1.items << i1
 s2.items << i2
