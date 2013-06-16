d9e5300523c10f8c527246a1aac3a466690e4133
Tanay Jalan <tanayjalan@gmail.com>
2013-06-16 16:27:34 -0400

    functionality pretty much summed up

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,5 @@ class Item < ActiveRecord::Base
   belongs_to :sale
   
   validates_numericality_of :price, :less_than => 4294967296
+  validates_numericality_of :price, :greater_than_or_equal_to => 0 
 end
