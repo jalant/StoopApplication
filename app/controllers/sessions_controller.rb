634ea8e274dcff2488941a778dfda6638966aa8c
Tanay Jalan <tanayjalan@gmail.com>
2013-06-16 00:23:07 -0400

    added some more random stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,20 @@ class SessionsController < ApplicationController
     puts session
   end
 
-  def create
+ def create
     @user = User.find_by_email(params[:email])
-    authenticated_user = @user.authenticate(params[:password])
-    if authenticated_user
-      session[:user_id] = authenticated_user.id
-      redirect_to root_path
+    if @user
+      authenticated_user = @user.authenticate(params[:password])
+      if authenticated_user
+        session[:user_id] = authenticated_user.id
+        redirect_to root_path
+      else
+        flash[:fail] = "We're sorry.  Either your email or password was incorrect"
+        render :new
+      end
     else
-      
-      flash[:fail] = "We're sorry.  Either your email or password was incorrect"
-      render :new
+      flash[:fail] = "You don't currently have an account.  Please create an account now!"
+      redirect_to new_user_path
     end
   end
 
